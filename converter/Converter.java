228e223c59aaa9e10a69cf1bd17f04dc9d3d5496
oceaquaris <oceaquarisoxyheloris@gmail.com>
2015-05-02 21:03:20 -0400

    Remove generic typing for simplicity and ease of use for students.

--- converter/Converter.java
+++ converter/Converter.java
@@ -1,20 +1,35 @@
 package converter;
 
-import java.io.Reader;
+//exceptions
+import java.io.FileNotFoundException;
+//standard io libraries
+import java.io.FileReader;
+import java.io.BufferedReader;
+
+//our libraries
+import reporter.ReportData;
 
 /**
- * Abstract class representing an object that converts some data from a Reader into a T object
+ * Abstract class representing an object that reads a file and
+ * converts the data within the file to a ReportData object.
  */
-public abstract class Converter<T> 
+public abstract class Converter
 {
-	protected Reader reader = null;
-	protected T data = null;
+	//see: http://www.homeandlearn.co.uk/java/read_a_textfile_in_java.html
+	protected BufferedReader reader = null;
+	protected ReportData data = null;
 	
-	public Converter() { }
+	public Converter() 
+	{
+		//An empty constructor; used to create a Converter object, itself.
+	}
+
 	/**
 	 * Creates a converter and loads the specified reader
+	 * @param reader
+	 *		BufferedReader for this object to use.
 	 */
-	public Converter(Reader reader)
+	public Converter(BufferedReader reader)
 	{
 		this.reader = reader;
 	}
@@ -22,23 +37,26 @@ public abstract class Converter<T>
 	/**
 	 * Creates a converter and loads the specified file
 	 * @param fileName
-	 *		String path to the file
-	 * @throws java.io.FileNotFoundException
+	 *		String path to the file to be loaded.
+	 * @throws FileNotFoundException
+	 *		Throws this if the file doesn't exist.
 	 */
-	public Converter(String fileName) throws java.io.FileNotFoundException
+	public Converter(String fileName) throws FileNotFoundException
 	{
-		this(new java.io.FileReader(fileName));
+		this( new BufferedReader( new FileReader(fileName) ) );
 	}
 
 	/** 
-	 * Converts the data contained in the reader to a POJO representation of the Reader
+	 * Converts the data contained in the reader to a ReportData
+	 * object representation of file read by the BufferedReader.
+	 * @return A pointer to the object created.
 	 */
-	public abstract T convertData();
+	public abstract ReportData convertData();
 	
 	/**
-	 * Returns the current POJO representation
+	 * Returns the current ReportData representation of the converted data.
 	 */
-	public T getData()
+	public ReportData getData()
 	{
 		return data;
 	}
