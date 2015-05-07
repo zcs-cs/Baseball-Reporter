7c5ae1e068184b581ac81ca4c9e3b7fe76dc58c6
oceaquaris <oceaquarisoxyheloris@gmail.com>
2015-05-06 21:36:10 -0400

    Template for csv converter added.

---
+++ converter/CSVConverter.java
@@ -0,0 +1,24 @@
+package converter;
+
+//imports
+
+
+public class CSVConverter extends Converter
+{
+	/**
+	 * Instance Data
+	 */
+	//TODO: instance data?
+	
+	/**
+	 * TODO: all 3 constructors
+	 */
+	
+	/**
+	 * TODO: convertData() implementation
+	 */
+	 
+	/**
+	 * TODO: toString()
+	 */
+}
