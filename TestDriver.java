da6bf67ea6a17bfed0bd15ef2be28b8708740364
Elliot Berman <elliot@elliotjb.com>
2015-05-07 13:23:33 -0400

    Updated documentation for Program and TestDriver
    [ci skip]

--- TestDriver.java
+++ TestDriver.java
@@ -2,7 +2,8 @@ import java.io.FileReader;
 import reporter.*;
 
 /**
- * Test Driver class.
+ * Test Driver class. Changes to this file will be ignored by Git. 
+ * This class may be updated as the the main driver from the Program class is developed. 
  */
 public class TestDriver 
 {
