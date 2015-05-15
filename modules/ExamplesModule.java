a6a94015f14f497be3d765f2e97f40774e27360c
Hunter Bobeck <hdbipod@gmail.com>
2015-05-15 09:57:24 -0400

    specific runs fix and description improvements

--- modules/ExamplesModule.java
+++ modules/ExamplesModule.java
@@ -26,31 +26,27 @@ public class ExamplesModule extends ReportModule<BaseballData>
         String[] weatherDescriptions = Array.permute(new String[] {"", "quite"}, new String[] {"freezing", "frigid", "cold", "cool", "refreshing", "moderate", "warm", "hot", "sweaty", "unbearable"});
         int weatherDescriptionIndex = (data.temperature() > 100) ? weatherDescriptions.length : (int) Function.line(0, 0, 100, weatherDescriptions.length, data.temperature());
         return "It took place at "+data.location()+" in "+data.city()+".\nThe temperature was a "+weatherDescriptions[weatherDescriptionIndex]+" "+data.temperature()+" degrees.\nrainfall: " + data.rainfall() + 
-              "\ndate: " + data.date() + 
+              "\nIt lasted for "+data.innings()+" innings.";
               "\nstart time: " + data.startTime() + 
               "\nminutes: " + data.minutes() + 
-              "\ninnings: " + data.innings();
     }
     // method calling examples - team statistics [including 'Random.text_of()' example] //
     private String teamStatisticsExample()
     {
         String statement = "The " + data.teamName(true) + ", or '" + data.teamDemonym(true) + "' for short, are from " + data.teamHomecity(true) + ". ";
-        statement += "\n";
         statement += data.teamResult(true) ? "They won their game!" : "They lost their game!";
         int[] teamInningScores = data.teamInningScores(true);
         String[] ordinals = {"first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth"};
         for (int i = 0; i < teamInningScores.length; i++)
         {
-            statement += "\nThe score for the " + ordinals[i] + " inning was " + teamInningScores[i] + ".";
+            statement += " The score for the " + ordinals[i] + " inning was " + teamInningScores[i] + ".";
         }
-        statement += "\n";
-        statement += Random.text_of(new String[] { 
+        statement += " "+Random.text_of(new String[] { 
             "Their total score was " + data.teamScore(true), 
             "The final tally for the team was " + data.teamScore(true)
             }) + ".";
-        statement += "\n";
-        statement += "This puts their standing at " + data.teamStandingsPosition(true) + 
-                    ".\nThey have had " + data.teamStandingsWins(true) + " wins.\nThey have had a " + data.teamStandingsStreak(true)+" game win streak.\nThey have had " + data.teamStandingsStreak(true) + " losses.";
+        statement += " This puts their standing at " + data.teamStandingsPosition(true) + 
+                    ". They have had " + data.teamStandingsWins(true) + " wins. They have had a " + data.teamStandingsStreak(true)+" game win streak. They have had " + data.teamStandingsStreak(true) + " losses.";
         return statement;
     }
     // method calling examples - team players statistics [including 'Text.capitalize()' and 'Function.line()' examples] //
@@ -70,14 +66,16 @@ public class ExamplesModule extends ReportModule<BaseballData>
         statement += teamDemonymCapitalized + " players' injuries' games missed: " + Array.text(data.teamPlayersInjuriesGamesMissed(true));
         statement += teamDemonymCapitalized + " players' injuries' types: " + Array.text(data.teamPlayersInjuriesTypes(true));
         statement += teamDemonymCapitalized + " players' injuries' locations: " + Array.text(data.teamPlayersInjuriesLocations(true));
-        statement += "\n";
+        statement += "\n\n";
         
         // specific player talk //
         String hitsPluralization = (data.teamPlayersHits(true)[0] == 1) ? "hit" : "hits";
         String[] runDescriptions = new String[] {"no", "one good", "two", "several", "four", "many"};
-        int runDescriptionIndex = (data.teamPlayersRuns(true)[0] > 5) ? 5 : Function.line(0, 0, runDescriptions.length, 5, data.teamPlayersRuns(true)[0]);
+        int runDescriptionIndex = (data.teamPlayersRuns(true)[0] > 5) ? 5 : Function.line(0, 0, 5, runDescriptions.length, data.teamPlayersRuns(true)[0]);
         String runsDescription = runDescriptions[runDescriptionIndex];
         String runPunctuation = (runDescriptionIndex <= 2) ? "." : "!";
-        return data.teamPlayersNames(true)[0]+" got "+data.teamPlayersHits(true)[0]+" "+hitsPluralization+" out of "+data.teamPlayersBats(true)[0]+" bats. He achieved "+runsDescription+" ("+data.teamPlayersRuns(true)[0]+") runs"+runPunctuation;
+        statement += data.teamPlayersNames(true)[0]+" got "+data.teamPlayersHits(true)[0]+" "+hitsPluralization+" out of "+data.teamPlayersBats(true)[0]+" bats. He achieved "+runsDescription+" ("+data.teamPlayersRuns(true)[0]+") runs"+runPunctuation;
+        
+        return statement;
     }
 }
