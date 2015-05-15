ceed96e0d5b1653de920c2df204080ba334370b4
Hunter Bobeck <hdbipod@gmail.com>
2015-05-15 09:33:28 -0400

    game result title generation
    - now generates a title based on the game result
    - the date is printed on the next line
    - removed ReportData referencing comment to put it into ExamplesModule

--- modules/TitleModule.java
+++ modules/TitleModule.java
@@ -1,6 +1,7 @@
 package modules;
 
 import data.BaseballData;
+import utilities.*;
 
 public class TitleModule extends ReportModule<BaseballData>
 {
@@ -9,9 +10,30 @@ public class TitleModule extends ReportModule<BaseballData>
 	 */
 	public String generate()
 	{
-	    //Please see the interface data/ReportData for method listings.
-	    return "The game was a great success with " + data.innings() +  " innings!";
-	    //Previous work below.
-		//return "The game was a great success with " + data.getInnings() +  " innings!";
+	    String winnerTeam, loserTeam, scores;
+	    int scoreDifferece;
+	    // prep ~"[Team A]s [Defeat] [Team B]s" output //
+	    if (data.teamResult(true))
+	    {
+	    	winnerTeamDemonym = data.teamDemonym(true);
+	    	loserTeamDemonym = data.teamDemonym(false);
+	    	scoreDifference = data.teamScore(true) - data.teamScore(false);
+	    	scores = data.teamScore(true)+"–"+data.teamScore(false);
+	    }
+	    // prep ~"[Team B]s Defeat [Team A]s" output //
+	    else if (data.teamResult(false))
+	    {
+	    	winnerTeamDemonym = data.teamDemonym(false);
+	    	loserTeamDemonym = data.teamDemonym(true);
+	    	scoreDifference = data.teamScore(false) - data.teamScore(true);
+	    	scores = data.teamScore(false)+"–"+data.teamScore(true);
+	    }
+	    // return either "[Team A]s Tie [Team B]s" or versa //
+	    else
+	    {
+	    	Boolean firstTeam = Random.binary();
+	    	return data.teamDemonym(firstTeam)+" Tie "+data.teamDemonym(!firstTeam);
+	    }
+	    return winnerTeamDemonym+" Defeat "+loserTeamDemonym+" "+scores+"\n"+data.date();
 	}
 }
