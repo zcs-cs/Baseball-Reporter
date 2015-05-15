3bc606b9d9ca3cbc6410e475aa0ca00635c742f4
jgeiser47 <jgeiser47@ymail.com>
2015-05-15 14:12:07 -0400

    update stupid module

--- modules/ExtraInningsModule.java
+++ modules/ExtraInningsModule.java
@@ -1,4 +1,5 @@
 package modules;
+
 import data.*;
 import utilities.*;
 
@@ -6,49 +7,49 @@ public class ExtraInningsModule extends ReportModule<BaseballData>
 {
     public String generate ()
     {
-        int numInnings = 10;
-        String team1 = "Angels";
-        String team2 = "Balls";
+        //int innings = 10;
+        //String team1 = this.teamName(true);
+        //String team2 = teamName(false);
         
-        if (numInnings > 9)
+        if (data.innings() > 9)
         {
             String x;
             int a = (int) (Math.random() * 5);
             if (a == 0)
             {
-                x = "The " + team1 + " beat the " + team2 + " in a " + numInnings + " inning game today.";
+                x = "The " + team1 + " beat the " + team2 + " in a " + innings + " inning game today.";
             }
             else if (a ==1)
             {
-                x = "The game went to " + numInnings + " innings.";
+                x = "The game went to " + innings + " innings.";
             }
             
             else if (a == 2)
             {
-                x = "After the first nine innings, the game lasted " + (numInnings - 9) +
+                x = "After the first nine innings, the game lasted " + (innings - 9) +
                     " more innings.";
             }
             else if (a == 3)
             {
-                x = "Fans got more than they payed for today as the game lasted " + (numInnings) +
+                x = "Fans got more than they payed for today as the game lasted " + (innings) +
                 " innings.";
             }
             else
             {
-                x = "The fans were tossing back the bud lights in this " + (numInnings) + " inning thriller.";
+                x = "The fans were tossing back the bud lights in this " + (innings) + " inning thriller.";
             }
         
     
               
                 
-            if (numInnings > 33 )
+            if (innings > 33 )
             {
                 x = x.concat (" The previous record for the longest MLB game was 33 innings, however " +
                 " that record has been broken today as the " + team1 + " beat the " + team2 + 
-                " in a " + numInnings + " inning affair.");
+                " in a " + innings + " inning affair.");
                
             }
-            else if (numInnings > 13)
+            else if (innings > 13)
             {
                 x = x.concat (" The record for the longest MLB game " + 
                 " is a 33 inning game between the Chicago White Sox and the Milwaukee Brewers. " +
