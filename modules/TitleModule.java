0f104f9ea85f75017f0414b62534ba72aa245559
Hunter Bobeck <hdbipod@gmail.com>
2015-05-15 09:44:24 -0400

    removed obsolete scoreDifference

--- modules/TitleModule.java
+++ modules/TitleModule.java
@@ -11,13 +11,11 @@ public class TitleModule extends ReportModule<BaseballData>
 	public String generate()
 	{
 	    String winnerTeamDemonym, loserTeamDemonym, scores;
-	    int scoreDifference;
 	    // prep ~"[Team A]s [Defeat] [Team B]s" output //
 	    if (data.teamResult(true))
 	    {
 	    	winnerTeamDemonym = data.teamDemonym(true);
 	    	loserTeamDemonym = data.teamDemonym(false);
-	    	scoreDifference = data.teamScore(true) - data.teamScore(false);
 	    	scores = data.teamScore(true)+"–"+data.teamScore(false);
 	    }
 	    // prep ~"[Team B]s Defeat [Team A]s" output //
@@ -25,7 +23,6 @@ public class TitleModule extends ReportModule<BaseballData>
 	    {
 	    	winnerTeamDemonym = data.teamDemonym(false);
 	    	loserTeamDemonym = data.teamDemonym(true);
-	    	scoreDifference = data.teamScore(false) - data.teamScore(true);
 	    	scores = data.teamScore(false)+"–"+data.teamScore(true);
 	    }
 	    // return either "[Team A]s Tie [Team B]s" or versa //
