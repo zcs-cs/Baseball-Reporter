b148bb277b2680ad5cecbdf5e5ce1c71c4184324
unknown <scott-eckart@ZHS-S-17031.SASI.zcs.k12.in.us>
2015-05-15 08:55:08 -0400

    Added BigInningModule

---
+++ modules/BigInningModule.java
@@ -0,0 +1,20 @@
+package modules;
+
+import data.BaseballData;
+/**
+ * Zach, Abbey, and Scott -- 1st Period
+ */
+public class BigInningModule extends ReportModule<BaseballData>
+{
+   
+    // Constructor
+    public BigInningModule(BaseballData data)
+    {
+       super(data);
+    }
+    
+    public String generate()
+    {
+        return "";
+    }
+}
