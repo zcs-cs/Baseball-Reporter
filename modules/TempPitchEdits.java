d58e26e2104182963219aa2acf57016e4388e6e3
oceaquaris <oceaquarisoxyheloris@gmail.com>
2015-05-16 11:46:05 -0400

    Temporary changes made to heler class. Please change as needed.

--- modules/TempPitchEdits.java
+++ modules/TempPitchEdits.java
@@ -9,15 +9,39 @@ package modules;
  */
 public class TempPitchEdits
 {
+   private int strikeouts1, strikeouts2, battersFaced1, battersFaced2, runs1, runs2;
+   private String Name1, Name2, Team1, Team2;
+   
+   public TempPitchEdits(int strikeouts1, int strikeouts2, 
+                         int battersFaced1, int battersFaced2,
+                         int runs1, int runs2,
+                         String Name1, String Name2,
+                         String Team1, String Team2)
+   {
+       this.strikeouts1 = strikeouts1;
+       this.strikeouts2 = strikeouts2; 
+       this.battersFaced1 = battersFaced1;
+       this.battersFaced2 = battersFaced2;
+       this.runs1 = runs1;
+       this.runs2 = runs2;
+       this.Name1 = Name1;
+       this.Name2 = Name2;
+       this.Team1 = Team1;
+       this.Team2 = Team2;
+   }
+   
+   //Temporary fix above. Please change as needed.
+   
    public String betterGame()
     {
+     String output = "";
       if (strikeouts1 > strikeouts2 && battersFaced1 > battersFaced2)
       {
         if(strikeouts1 >= battersFaced1 / 2)
         {
             output += Name1 + " destroyed the " + Team2 + ", with more than half of the batters faced being struck out.";
         }
-        else if(strikeouts1 >= battersfaced1 / 3)
+        else if(strikeouts1 >= battersFaced1 / 3)
         {
             output += Name1 + "Had a good game against" + Team2 + ".";
         }
@@ -33,7 +57,7 @@ public class TempPitchEdits
         {
             output += Name2 + " destroyed the " + Team1 + ", with more than half of the batters faced being struck out.";
         }
-        else if(strikeouts1 >= battersfaced1 / 3)
+        else if(strikeouts1 >= battersFaced1 / 3)
         {
             output += Name2 + "Had a good game against" + Team1 + ".";
         }
@@ -44,11 +68,11 @@ public class TempPitchEdits
       }
      else 
      {
-        if(strikeouts1 > Strikeouts2)
+        if(strikeouts1 > strikeouts2)
         {
             output += Name1 + " struck out the most batters";
         }
-        else if(strikeouts1 < Strikeouts2)
+        else if(strikeouts1 < strikeouts2)
         {
             output += Name2 + "Struck out the most batters";
         }
