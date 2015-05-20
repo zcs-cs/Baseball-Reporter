b686afcd9b296c7bce4c8ba26d799f51abd49f94
Alex van de Sandt <alexvds17@gmail.com>
2015-05-20 08:40:40 -0400

    Trying last commit again

---
+++ modules/SavesAndReliefsModule.java
@@ -0,0 +1,21 @@
+package modules;
+
+import data.BaseballData;
+import utilities.*;
+
+/**
+ * Write a description of class SavesAndReliefsModule here.
+ * 
+ * @author Alex Han, Jacob Quick, Matt Sibila, Alex van de Sandt
+ */
+public class SavesAndReliefsModule  extends ReportModule<BaseballData> {
+    public SavesAndReliefsModule (BaseballData data) {
+        super(data);
+    }
+    
+    public String generate () {
+        
+        
+        return "Nothing of interest";
+    }
+}
