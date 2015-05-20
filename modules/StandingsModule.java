ad27976ee08d0900ab87c341f3c4c4514a33e838
Claire <claire-berman@stumail.zcs.k12.in.us>
2015-05-20 08:26:52 -0400

    Changed Sentences

--- modules/StandingsModule.java
+++ modules/StandingsModule.java
@@ -4,37 +4,37 @@ import java.util.Random;
 
 public class StandingsModule extends ReportModule<BaseballData>
 {
-     public StandingsModule(BaseballData data) { super(data); }
+    public StandingsModule(BaseballData data) { super(data); }
     /**
      *  * * * * * * * *
      *  Claire Berman *
      *  * * * * * * * *
      */
-    
+
     int teamAScore = data.teamScore(true);
     int teamBScore = data.teamScore(false);
-    
+
     int teamAWins = data.teamStandingsWins(true);
     int teamBWins = data.teamStandingsWins(false);
-    
+
     int teamALosses = data.teamStandingsLosses(true);
     int teamBLosses = data.teamStandingsLosses(false);
-    
+
     int teamAPCT = data.teamStandingsPCT(true);
     int teamBPCT = data.teamStandingsPCT(false);
     int teamCPCT = data.otherTeamStandingsPCT(true);
     int teamDPCT = data.otherTeamStandingsPCT(false);
-    
+
     int teamAPosition = data.teamStandingsPosition(true);
     int teamBPosition = data.teamStandingsPosition(false);
     int teamCPosition = data.otherTeamStandingsPosition(true);
     int teamDPosition = data.otherTeamStandingsPosition(false);
-    
+
     String teamADemonym = data.teamDemonym(true); //Indians
     String teamBDemonym = data.teamDemonym(false); //Bats
     String teamCDemonym = data.otherTeamDemonym(true); //Hens
     String teamDDemonym = data.otherTeamDemonym(false); //Clippers
-    
+
     String name;
     int function;
     Random generator = new Random();
@@ -64,21 +64,22 @@ public class StandingsModule extends ReportModule<BaseballData>
         else
         {
             if (function == 1)
-                return teamCDemonym + "are in position " + teamCPosition + ", " + teamDDemonym + " are in position " + teamDPosition + ", " + teamADemonym + " are in position " + teamAPosition + ", and " + teamBDemonym + " are in position " + teamBPosition + ", since the " + name + " won.";
+                return teamCDemonym + " stand in place " + teamCPosition + ", " + teamDDemonym + " stand in place " + teamDPosition + ", " + teamADemonym + " stand in place " + teamAPosition + ", and " + teamBDemonym + " stand in place " + teamBPosition + ", since the " + name + " won.";
             else if (function == 2)
-                return teamCDemonym + "are in position " + teamCPosition + ", " + teamADemonym + " are in position " + teamAPosition + ", " + teamDDemonym + " are in position " + teamDPosition + ", and " + teamBDemonym + " are in position " + teamBPosition + ", since the " + name + " won.";
+                return teamCDemonym + " stand in place " + teamCPosition + ", " + teamADemonym + " stand in place " + teamAPosition + ", " + teamDDemonym + " stand in place " + teamDPosition + ", and " + teamBDemonym + " stand in place " + teamBPosition + ", since the " + name + " won.";
             else if (function == 3)
-                return teamADemonym + "are in position " + teamAPosition + ", " + teamCDemonym + " are in position " + teamCPosition + ", " + teamDDemonym + " are in position " + teamDPosition + ", and " + teamBDemonym + " are in position " + teamBPosition + ", since the " + name + " won.";
+                return teamADemonym + " stand in place " + teamAPosition + ", " + teamCDemonym + " stand in place " + teamCPosition + ", " + teamDDemonym + " stand in place " + teamDPosition + ", and " + teamBDemonym + " stand in place " + teamBPosition + ", since the " + name + " won.";
             else if (function == 4)
-                return teamCDemonym + "are in position " + teamCPosition + ", " + teamDDemonym + " are in position " + teamDPosition + ", " + teamBDemonym + " are in position " + teamBPosition + ", and " + teamADemonym + " are in position " + teamAPosition + ", since the " + name + " won.";
+                return teamCDemonym + " stand in place " + teamCPosition + ", " + teamDDemonym + " stand in place " + teamDPosition + ", " + teamBDemonym + " stand in place " + teamBPosition + ", and " + teamADemonym + " stand in place " + teamAPosition + ", since the " + name + " won.";
             else if (function == 5)
-                return teamCDemonym + "are in position " + teamCPosition + ", " + teamBDemonym + " are in position " + teamBPosition + ", " + teamDDemonym + " are in position " + teamDPosition + ", and " + teamADemonym + " are in position " + teamAPosition + ", since the " + name + " won.";
+                return teamCDemonym + " stand in place " + teamCPosition + ", " + teamBDemonym + " stand in place " + teamBPosition + ", " + teamDDemonym + " stand in place " + teamDPosition + ", and " + teamADemonym + " stand in place " + teamAPosition + ", since the " + name + " won.";
             else if (function == 6)
-                return teamBDemonym + "are in position " + teamBPosition + ", " + teamCDemonym + " are in position " + teamDPosition + ", " + teamCDemonym + " are in position " + teamDPosition + ", and " + teamADemonym + " are in position " + teamAPosition + ", since the " + name + " won.";
+                return teamBDemonym + " stand in place " + teamBPosition + ", " + teamCDemonym + " stand in place " + teamDPosition + ", " + teamCDemonym + " stand in place " + teamDPosition + ", and " + teamADemonym + " stand in place " + teamAPosition + ", since the " + name + " won.";
             else
                 return "The standings are not available right now.";
         }
     }
+
     /**
      * Update wins, losses, and PCT based on score of game
      */
@@ -106,6 +107,7 @@ public class StandingsModule extends ReportModule<BaseballData>
             teamBPCT = (teamBWins / (teamBWins + teamBLosses)) + 100;
         }
     }
+
     /**
      * Find standings of each team based on PCT
      */
